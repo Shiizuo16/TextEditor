70227d08ec3b33cfbd73693d9fbb750e6d79a32f
Shiizuo <slicedheaven314@gmail.com>
2022-12-12 08:58:01 +0530

    Version 1.1
    Added open, new, save and save-as keyboard shortcuts.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QFont
+from PyQt5.QtGui import QFont, QKeySequence
+from PyQt5.QtCore import Qt
 from PyQt5 import uic
 import sys
 
@@ -7,6 +8,7 @@ class EditorGUI(QMainWindow):
 
     def __init__(self):
         self.current_file = None
+        self.stuff = str()
         super(EditorGUI, self).__init__()
         uic.loadUi("src\\editor.ui", self)
         self.show()
@@ -23,31 +25,55 @@ class EditorGUI(QMainWindow):
         self.actionSave.triggered.connect(self.save_file)
         self.actionSave_As.triggered.connect(self.save_file_as)
         self.actionClose.triggered.connect(self.closeEvent)
+        # Shortcuts
+        self.shortcut_open = QShortcut(QKeySequence('ctrl+o'), self)
+        self.shortcut_open.activated.connect(self.open_file)
+        self.shortcut_new = QShortcut(QKeySequence('ctrl+n'), self)
+        self.shortcut_new.activated.connect(self.new_file)
+        self.shortcut_save = QShortcut(QKeySequence('ctrl+s'), self)
+        self.shortcut_save.activated.connect(self.save_file)
+        self.shortcut_save_as = QShortcut(QKeySequence('ctrl+shift+s'), self)
+        self.shortcut_save_as.activated.connect(self.save_file_as)
 
     def change_size(self, size):
         self.plainTextEdit.setFont(QFont("Arial", size))
 
+    def check_stuff(self):
+        return True if self.plainTextEdit.toPlainText() == self.stuff else False
+
     def open_file(self):
+        if not self.check_stuff():
+            # Save file dialog box
+            dialog = QMessageBox()
+            dialog.setText("Do you want to save your work?")
+            dialog.addButton(QPushButton("Yes"), QMessageBox.YesRole)  # 0
+            dialog.addButton(QPushButton("No"), QMessageBox.NoRole)  # 1
+            dialog.addButton(QPushButton("Cancel"), QMessageBox.RejectRole)  # 2
+
+            answer = dialog.exec_()
+
+            if answer == 0:
+                self.save_file()
+            elif answer == 2:
+                return 0
+
         options = QFileDialog.Options()
-        filename = QFileDialog.getOpenFileName(self, "Open File", "", "Text Files (*.txt);;Python Files (*.py);;All files (*)", options=options)[0]
+        filename = QFileDialog.getOpenFileName(self, "Open File", "Files\\", "Text Files (*.txt);;Python Files (*.py);;All files (*)", options=options)[0]
         if filename != "":
             with open(filename, "r") as file:
-                self.plainTextEdit.setPlainText(file.read())
-            self.current_file = filename
+                stuff = file.read()
+                self.plainTextEdit.setPlainText(stuff)
+                self.current_file = filename
+                self.stuff = stuff
 
     def save_file(self):
         if self.current_file is None:
-            print('file does not exist')
             self.save_file_as()
         else:
-            print('file exists')
             with open(self.current_file, 'w') as file:
-                print('file opened')
                 stuff = self.plainTextEdit.toPlainText()
-                print(stuff)
                 file.write(stuff)
-                print('file written')
-        print('file saved')
+                self.stuff = stuff
 
     def save_file_as(self):
         options = QFileDialog.Options()
@@ -55,11 +81,20 @@ class EditorGUI(QMainWindow):
         if filename != "":
             with open(filename, "w") as file:
                 stuff = self.plainTextEdit.toPlainText()
-                print(stuff)
                 file.write(stuff)
-            self.current_file = filename
+                self.current_file = filename
+                self.stuff = stuff
 
     def new_file(self):
+        # If the file is empty or no change has been made --> don't save
+        if self.plainTextEdit.toPlainText() == "" or self.check_stuff():
+            self.plainTextEdit.setPlainText("")
+            self.current_file = None
+            self.stuff = str()
+            return 0
+
+
+        # Save file dialog box
         dialog = QMessageBox()
         dialog.setText("Do you want to save your work?")
         dialog.addButton(QPushButton("Yes"), QMessageBox.YesRole)  # 0
@@ -70,15 +105,19 @@ class EditorGUI(QMainWindow):
 
         if answer == 0:
             self.save_file()
-            self.plainTextEdit.setPlainText("")
-            self.current_file = None
-        elif answer == 1:
-            self.plainTextEdit.setPlainText("")
-            self.current_file = None
+        elif answer == 2:
+            return 0
+        self.plainTextEdit.setPlainText("")
+        self.current_file = None
+        self.stuff = str()
 
     def closeEvent(self, event):
-        if self.plainTextEdit.toPlainText() == "":
+        if self.plainTextEdit.toPlainText() == "": # If file is empty, just exit
             sys.exit()
+        elif self.check_stuff(): # If no change has been made, just exit
+            sys.exit()
+
+        # Save File dialog box
         dialog = QMessageBox()
         dialog.setText("Do you want to save your work?")
         dialog.addButton(QPushButton("Yes"), QMessageBox.YesRole)           # 0
